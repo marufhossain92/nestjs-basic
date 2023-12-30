552f8fb6a314841486bc17d45a069f1e21207187
Maruf Hossain <emarufh@gmail.com>
2023-12-30 19:47:03 +0600

    CRUD implemented

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,93 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'graham@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Ervin Smith',
+      email: 'smith@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'Bob Doe',
+      email: 'bob@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'Patricia Green',
+      email: 'green@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'Crystal Bean',
+      email: 'ben@gmail.com',
+      role: 'ADMIN',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const userByHighestId = [...this.users].sort(
+      (a: any, b: any) => b.id - a.id,
+    );
+
+    const newUser = {
+      id: userByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updateUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updateUser };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removeUser = this.findOne(id);
+
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return removeUser;
+  }
+}
